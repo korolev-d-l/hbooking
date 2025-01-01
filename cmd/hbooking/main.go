9f6121c6b7a57582205521779a669f35cee2bf65
Dmitry Korolev <chameleonweb2012@gmail.com>
2025-01-01 21:50:57 +0300

    - os.Exit(1) for errors

--- cmd/hbooking/main.go
+++ cmd/hbooking/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log/slog"
+	"os"
 
 	"github.com/spatecon/hbooking/internal/pkg/app"
 )
@@ -10,10 +11,12 @@ func main() {
 	ap, err := app.NewApp()
 	if err != nil {
 		slog.Error("failed to create app", "error", err)
+		os.Exit(1)
 		return
 	}
 
 	if err = ap.Run(); err != nil {
 		slog.Error("failed to run app", "error", err)
+		os.Exit(1)
 	}
 }
